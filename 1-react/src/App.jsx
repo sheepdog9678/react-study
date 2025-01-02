b4dca2f92a4628bfedb89d0332463b746e07e571
sheepdog13 <sheepdog13@naver.com>
2025-01-03 03:57:31 +0900

    feat : btn 컴포넌트 - 컴포넌트로 나누어서 만들면 유지 보수가 편리해진다 - 컴포넌트를 만들 때는 재사용성을 신경 쓰면서 만든다

--- 1-react/src/App.jsx
+++ 1-react/src/App.jsx
@@ -1,3 +1,47 @@
-const App = () => <>1-react</>;
+import Button from "./components/Button";
+
+const App = () => (
+  <div className="ProductPage">
+    <div className="Page">
+      <header>
+        <h1>메뉴목록</h1>
+      </header>
+      <main>
+        <ul>
+          <li>
+            <div className="ProductItem">
+              <div className="description">
+                <h2>고소한 바질 파스타</h2>
+                <div>6,000원</div>
+                <Button
+                  styleType={"brand"}
+                  onClick={() => {
+                    console.log("장바구니로 이동");
+                  }}
+                >
+                  주문하기
+                </Button>
+              </div>
+              <div className="thumbnail">
+                <img
+                  src="./images/menu-고소한바질파스타.jpg"
+                  alt="고소한바질파스타 6,000원"
+                />
+              </div>
+            </div>
+          </li>
+        </ul>
+      </main>
+      <footer>
+        <nav className="Navbar">
+          <a className="active" href="#">
+            메뉴목록
+          </a>
+          <a href="#">주문내역</a>
+        </nav>
+      </footer>
+    </div>
+  </div>
+);
 
 export default App;
