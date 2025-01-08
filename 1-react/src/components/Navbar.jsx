58b9e818d2e2db96166b3d97ceeff81a3c261131
sheepdog13 <sheepdog13@naver.com>
2025-01-09 03:16:12 +0900

    feat : navbar 강사님 풀이 #2
    - 아직 hook을 배우지 않아서 아무런 동작을 구현하지 않았다
    - 다른 컴포넌트와 다르게 재사용이 불가능 해 보인다

--- 1-react/src/components/Navbar.jsx
+++ 1-react/src/components/Navbar.jsx
@@ -1,27 +1,10 @@
-import { useState } from "react";
-
 const Navbar = () => {
-  const [toggle, setToggle] = useState(true);
   return (
     <nav className="Navbar">
-      <a
-        className={toggle ? "active" : ""}
-        onClick={() => {
-          setToggle(true);
-        }}
-        href="#"
-      >
+      <a className="active" href="#">
         메뉴목록
       </a>
-      <a
-        className={toggle ? "" : "active"}
-        onClick={() => {
-          setToggle(false);
-        }}
-        href="#"
-      >
-        주문내역
-      </a>
+      <a href="#">주문내역</a>
     </nav>
   );
 };
