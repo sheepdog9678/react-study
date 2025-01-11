3359beaeb9e376b8f391e19a82049cf7a211ed31
sheepdog13 <sheepdog13@naver.com>
2025-01-11 16:24:10 +0900

    feat : Product page

---
+++ 1-react/src/pages/ProductPage/index.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import ProductApi from "shared/api/ProductApi";
+import Page from "../../components/Page";
+import Title from "../../components/Title";
+import Navbar from "../../components/Navbar";
+import ProductItem from "../../components/ProductItem";
+
+class ProductPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      productList: [],
+    };
+  }
+
+  componentDidMount() {
+    this.fetch();
+  }
+
+  async fetch() {
+    try {
+      const productList = await ProductApi.fetchProductList();
+      this.setState({ productList });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  render() {
+    return (
+      <div className="ProductPage">
+        <Page header={<Title>메뉴목록</Title>} footer={<Navbar />}>
+          <ul>
+            {this.state.productList.map((product) => (
+              <li key={product.id}>
+                <ProductItem product={product} />
+              </li>
+            ))}
+          </ul>
+        </Page>
+      </div>
+    );
+  }
+}
+
+export default ProductPage;
