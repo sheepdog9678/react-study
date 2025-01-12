f994b3bb005898ae5d1b26a0a7d3589be299a3bf
sheepdog9678 <sheepdog13@naver.com>
2025-01-12 17:45:07 +0900

    feat : OrderStatusCard #3

---
+++ 1-react/src/pages/OrderPage/components/OrderStatusCard.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import Card from "../../../components/Card";
+import Button from "../../../components/Button";
+const fakeData = {
+  deliveryAddress: "서울특별시 송파구 잠실동 1번지",
+  deliveryContact: "010-1111-2222",
+  deliveryPrice: 3000,
+  discountPrice: 2000,
+  id: "CACDA420",
+  messageToRider: "안전하게 오세요",
+  messageToShop: "포크는 주지 마세요",
+  name: "짜장면",
+  orderDate: "2025. 1. 12. 오후 5:00:38",
+  paymentMethod: "마이페이",
+  position: [60, 60],
+  productPrice: 6000,
+  status: "배달중",
+  totalPrice: 7000,
+};
+
+class OrderStatusCard extends Component {
+  render() {
+    return (
+      <div>
+        <Card
+          header={fakeData.name}
+          footer={
+            <>
+              <Button>전화</Button>
+              <Button>가게보기</Button>
+            </>
+          }
+          data={[
+            { term: "주문일시", description: fakeData.orderDate },
+            { term: "주문번호", description: fakeData.id },
+          ]}
+        ></Card>
+      </div>
+    );
+  }
+}
+
+export default OrderStatusCard;
--- 1-react/src/pages/OrderPage/index.jsx
+++ 1-react/src/pages/OrderPage/index.jsx
@@ -3,17 +3,14 @@ import Navbar from "../../components/Navbar";
 import Page from "../../components/Page";
 import Title from "../../components/Title";
 import Card from "../../components/Card";
+import OrderStatusCard from "./components/OrderStatusCard";
 
 class OrderPage extends Component {
   render() {
     return (
       <div className="OrderPage">
         <Page header={<Title>주문내역</Title>} footer={<Navbar />}>
-          <Card
-            header="헤더"
-            data={[{ term: "term", description: "" }]}
-            footer="푸터"
-          />
+          <OrderStatusCard />
         </Page>
       </div>
     );
