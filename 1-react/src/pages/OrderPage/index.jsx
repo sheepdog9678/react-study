69e3665394eb933fac5809bba77c773d920a906d
sheepdog9678 <sheepdog13@naver.com>
2025-01-23 15:24:49 +0900

    feat : OrderPage 상태 정의
    - Card 컴포넌트에서 api를 불러올 경우 3번 같은 내용을 중복으로
    - 들고 올 가능성이 있기 때문에 OrderPage에서 한 번만 불러온다

--- 1-react/src/pages/OrderPage/index.jsx
+++ 1-react/src/pages/OrderPage/index.jsx
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import OrderApi from "shared/api/OrderApi";
 import Navbar from "../../components/Navbar";
 import Page from "../../components/Page";
 import Title from "../../components/Title";
@@ -6,31 +7,36 @@ import OrderStatusCard from "./components/OrderStatusCard";
 import OrederPaymentCard from "./components/OrederPaymentCard";
 import OrderDeliveryCard from "./components/OrderDeliveryCard";
 
-const fakeData = {
-  deliveryAddress: "서울특별시 송파구 잠실동 1번지",
-  deliveryContact: "010-1111-2222",
-  deliveryPrice: 3000,
-  discountPrice: 2000,
-  id: "CACDA420",
-  messageToRider: "안전하게 오세요",
-  messageToShop: "포크는 주지 마세요",
-  name: "짜장면",
-  orderDate: "2025. 1. 12. 오후 5:00:38",
-  paymentMethod: "마이페이",
-  position: [60, 60],
-  productPrice: 6000,
-  status: "배달중",
-  totalPrice: 7000,
-};
-
 class OrderPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      order: null,
+    };
+  }
+  async fetch() {
+    try {
+      const order = await OrderApi.fetchMyOrder();
+      this.setState({ order });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+  componentDidMount() {
+    this.fetch();
+  }
   render() {
+    const { order } = this.state;
     return (
       <div className="OrderPage">
         <Page header={<Title>주문내역</Title>} footer={<Navbar />}>
-          <OrederPaymentCard data={fakeData} />
-          <OrderStatusCard data={fakeData} />
-          <OrderDeliveryCard data={fakeData} />
+          {order && (
+            <>
+              <OrederPaymentCard data={order} />
+              <OrderStatusCard data={order} />
+              <OrderDeliveryCard data={order} />
+            </>
+          )}
         </Page>
       </div>
     );
