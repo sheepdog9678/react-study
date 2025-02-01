976df80cb31a990e54b253df89c82d1e15165b77
sheepdog9678 <sheepdog13@naver.com>
2025-02-01 17:55:51 +0900

    feat : CartPage 레이아웃

--- 1-react/src/App.jsx
+++ 1-react/src/App.jsx
@@ -1,6 +1,7 @@
+import CartPage from "./pages/CartPage";
 import OrderPage from "./pages/OrderPage";
 import ProductPage from "./pages/ProductPage";
 
-const App = () => <OrderPage />;
+const App = () => <CartPage />;
 
 export default App;
---
+++ 1-react/src/pages/CartPage/index.jsx
@@ -0,0 +1,30 @@
+import Button from "../../components/Button";
+import Page from "../../components/Page";
+import ProductItem from "../../components/ProductItem";
+import Title from "../../components/Title";
+
+const CartPage = () => {
+  const fakeItem = {
+    id: "CACDA421",
+    name: "해물 계란 라면",
+    price: 6000,
+    thumbnail: "./images/menu-해물계란라면.jpg",
+  };
+
+  return (
+    <div className="CartPage">
+      <Page
+        header={<Title>장바구니</Title>}
+        footer={
+          <Button styleType={"brand-solid"} block={"block"}>
+            결제하기
+          </Button>
+        }
+      >
+        <ProductItem product={fakeItem} />
+      </Page>
+    </div>
+  );
+};
+
+export default CartPage;
