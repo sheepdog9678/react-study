ff6f85b80d3e62f3ba9bfbc8d0937c7c9d626d99
sheepdog9678 <sheepdog13@naver.com>
2025-02-24 21:10:30 +0900

    feat : orderpage 적용

--- 1-react/src/App.jsx
+++ 1-react/src/App.jsx
@@ -2,6 +2,6 @@ import CartPage from "./pages/CartPage";
 import OrderPage from "./pages/OrderPage";
 import ProductPage from "./pages/ProductPage";
 
-const App = () => <CartPage />;
+const App = () => <OrderPage />;
 
 export default App;
